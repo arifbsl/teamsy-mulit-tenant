0429966fa45539959489715fb27af19c67da899d
Arif Hossen <ahak.bsl@gmail.com>
2022-06-03 18:05:39 +0600

    Registration working fine

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,45 +1,61 @@
 @section('title', 'Create a new account')
-<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-  <div class="max-w-md w-full space-y-8">
-    <div>
-      <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Start your free trail</h2>
-      <p class="mt-2 text-center text-sm text-gray-600">
-        Or
-        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
-      </p>
-    </div>
-    <form wire:submit.prevent="register">
-      <div class="rounded-md shadow-sm -space-y-px">
-        <div>
-          <label for="name" class="sr-only">Name</label>
-          <input id="name" wire:model="name" type="text" autocomplete="name" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Full name">
-        </div>
-        <div>
-          <label for="companyName" class="sr-only">Company Name</label>
-          <input id="companyName" wire:model="companyName" type="text" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Company name">
-        </div>
-        <div>
-          <label for="email-address" class="sr-only">Email address</label>
-          <input id="email-address" wire:model="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
-        </div>
-        <div>
-          <label for="password" class="sr-only">Password</label>
-          <input id="password" wire:model="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
-        </div>
-      </div>
+<div>
+  <div class="min-h-screen bg-white flex">
+      <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div class="mx-auto w-full max-w-sm">
+              <div>
+                  <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+                  <h2 class="mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+                      Start your free trial
+                  </h2>
+              </div>
 
-      <div class="mt-8">
-        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            <!-- Heroicon name: solid/lock-closed -->
-            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-            </svg>
-          </span>
-          Register
-        </button>
+              <div class="mt-8">
+                  <div class="mt-6">
+                      <form wire:submit.prevent="register">
+                          <x-text-input
+                              wire:model.debounce.100ms="name"
+                              type="text"
+                              label="Name"
+                              :required="true"
+                              placeholder="my placeholder"
+                              class="mt-4" />
+                          <x-text-input
+                              wire:model.debounce.100ms="companyName"
+                              type="text"
+                              label="Company Name"
+                              :required="true"
+                              placeholder="my placeholder"
+                              class="mt-4" />
+                          <x-text-input
+                              wire:model.debounce.100ms="email"
+                              type="email"
+                              label="Email"
+                              :required="true"
+                              placeholder="kevin@kevinmckee.me"
+                              class="mt-4" />
+                          <x-text-input
+                              wire:model.debounce.100ms="password"
+                              type="password"
+                              label="Password"
+                              :required="true"
+                              placeholder=""
+                              class="mt-4" />
+
+                          <div class="mt-6">
+                              <span class="block w-full rounded-md shadow-sm">
+                                  <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                      Register
+                                  </button>
+                              </span>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
       </div>
-    </form>
+      <div class="hidden lg:block relative w-0 flex-1">
+          <img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="" />
+      </div>
   </div>
 </div>
