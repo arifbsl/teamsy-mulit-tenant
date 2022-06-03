75f250412511b06e791cf242721284a51757dbb4
Arif Hossen <ahak.bsl@gmail.com>
2022-06-03 20:03:04 +0600

    prepare for 8

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -37,4 +38,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function avatarUrl()
+    {
+        if ($this->photo) {
+            return Storage::disk('public')->url($this->photo);
+        }
+        return null;
+    }
+
+    public function isAdmin()
+    {
+        return $this->role == 'Admin';
+    }
+
+    public function isHR()
+    {
+        return $this->role == 'Human Resources';
+    }
 }
--- resources/views/livewire/add-user.blade.php
+++ resources/views/livewire/add-user.blade.php
@@ -1,20 +1,20 @@
 <div>
   <form wire:submit.prevent="submit">
     <div class="grid grid-cols-6 gap-6">
-      <x-text-input wire:model="name" label="Name" :required="true" placeholder="Jeffrey Way" class="col-span-6 sm:col-span-3" />
+      <x-text-input wire:model="name" label="Name" placeholder="Jeffrey Way" class="col-span-6 sm:col-span-3" />
 
-      <x-text-input wire:model="email" type="email" label="Email" :required="true" placeholder="jeffrey@laracasts.com" class="col-span-6 sm:col-span-3" />
+      <x-text-input wire:model="email" type="email" label="Email" placeholder="jeffrey@laracasts.com" class="col-span-6 sm:col-span-3" />
 
       <div class="col-span-6 sm:col-span-2">
         <label for="department" class="block text-sm font-medium leading-5 text-gray-700">Department</label>
         <select wire:model="department" id="department" class="mt-1 block form-select w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
-          <option value="human_resources">Human Resources</option>
-          <option value="marketing">Marketing</option>
-          <option value="information_technology">Information Technology</option>
+          <option value="Human Resources">Human Resources</option>
+          <option value="Marketing">Marketing</option>
+          <option value="Information Technology">Information Technology</option>
         </select>
       </div>
 
-      <x-text-input wire:model="title" label="Title" :required="true" placeholder="Instructor" class="col-span-6 sm:col-span-3" />
+      <x-text-input wire:model="title" label="Title" placeholder="Instructor" class="col-span-6 sm:col-span-3" />
 
       <div class="col-span-6">
         <label class="block text-sm leading-5 font-medium text-gray-700 mb-2">
@@ -32,16 +32,27 @@
               </svg>
             @endif
           </div>
-
           <div>
             <input type="file" wire:model="photo">
-
-            @error('photo')
-              <span class="error">{{ $message }}</span>
-            @enderror
           </div>
-
+          @error('photo')
+            <div class="text-sm text-red-500 mt-2">{{ $message }}</div>
+          @enderror
         </div>
+      </div>
+
+      <div class="col-span-6">
+        <label class="block text-sm leading-5 font-medium text-gray-700 mb-2">
+          Application
+        </label>
+        <div class="flex flex-items-center">
+          <div>
+            <input type="file">
+          </div>
+        </div>
+        @error('application')
+          <div class="text-sm text-red-500 mt-2">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="col-span-6 sm:col-span-3">
@@ -55,21 +66,18 @@
       <div class="col-span-6 sm:col-span-2">
         <label for="role" class="block text-sm font-medium leading-5 text-gray-700">Role</label>
         <select wire:model="role" id="role" class="mt-1 block form-select w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
-          <option value="admin">Admin</option>
-          <option value="manager">Manager</option>
-          <option value="member">Team Member</option>
+          <option value="Admin">Admin</option>
+          <option value="Manager">Manager</option>
+          <option value="Member">Team Member</option>
         </select>
       </div>
 
-
     </div>
-
 
     <div class="mt-8 border-t border-gray-200 pt-5">
       @if (session()->has('success'))
         {{ session('success') }}
       @endif
-
       <div class="flex justify-end">
         <span class="inline-flex rounded-md shadow-sm">
           <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
