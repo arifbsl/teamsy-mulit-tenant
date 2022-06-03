a856415083bc0ae7c5c3249d35903246f7255eb2
Arif Hossen <ahak.bsl@gmail.com>
2022-06-03 16:27:35 +0600

    Create and Use BelongsToTenant in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,7 @@
 
 namespace App\Models;
 
-use App\Scopes\TenantScope;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, BelongsToTenant;
 
     /**
      * The attributes that are mass assignable.
@@ -43,13 +42,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::addGlobalScope(new TenantScope);
-    }
 }
